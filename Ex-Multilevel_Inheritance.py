52db819e2525d1dc51aa750c7efac53ac16c0952
mehular0ra <22mehularora22@gmail.com>
2022-02-28 15:31:40 +0530

    oops

---
+++ Ex-Multilevel_Inheritance.py
@@ -0,0 +1,31 @@
+class ElectronicDevice: 
+    name = 'Pager'
+    hardDevide = True
+    def __init__(self, MACAddress):
+        MACAddress = MACAddress
+
+    def sendText(string):
+        print(string, " is send to the user.")
+        
+
+class PocketGadget: 
+    name = 'Mobile'
+    def __init__(self, size):
+        size = size
+
+    def makeCall(ph_no):
+        print("Call made to this number. ")
+
+class Phone:
+    name = 'SmartPhone'
+    def __init__(self, network_type, internet_speed):
+        network_type = network_type
+        internet_speed = internet_speed
+
+    def sendMail(emailID, mail_body):
+        print(f"'{mail_body} send to {emailID}")
+
+
+
+
+
